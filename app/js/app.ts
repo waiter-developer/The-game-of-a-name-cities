9a14d74322acd662dc25e35fe26f82ffed9db0d9
waiter-developer <yatsunenko90@gmail.com>
2020-11-12 13:50:37 +0200

    test ts

--- app/js/app.ts
+++ app/js/app.ts
@@ -1,4 +1,4 @@
-"use strict";
+
 // class User {
 //     constructor(public firstName: string, private readonly lastName: string = 'Good user name') {
 //         this.firstName = firstName;
@@ -13,31 +13,44 @@
 //
 // let user1: User = new User('Jow', 'Mick');
 // user1.print();
-var Point = /** @class */ (function () {
-    function Point(x, y) {
+
+
+class Point {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
     }
-    return Point;
-}());
-var Grid = /** @class */ (function () {
-    function Grid() {
-        this.points = [];
-    }
-    Grid.origin = { x: 0, y: 0 };
-    return Grid;
-}());
-var grid1 = new Grid();
-console.log(grid1);
-function drawSomething(rect) {
-    if (rect.name) {
-        console.log("Name " + rect.name);
-    }
-    console.log(rect.height + " " + rect.height);
 }
-var rect1 = {
+
+class Grid {
+    public points: Point[] = [];
+    public static origin: Point = { x: 0, y: 0};
+}
+
+let grid1 = new Grid()
+console.log(grid1);
+
+interface Something {
+    name?: string,
+    width: number,
+    height: number
+}
+
+function drawSomething(rect: Something) {
+    if(rect.name){
+        console.log(`Name ${rect.name}`)
+    }
+
+    console.log(`${rect.height} ${rect.height}`)
+}
+
+let rect1 = {
     name: 'Rectangle1',
     width: 123,
     height: 45
-};
+}
+
 drawSomething(rect1);
